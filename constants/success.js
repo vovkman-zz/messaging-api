f40093187a7adfd10cd8947e961579dda5f01454
Liam Vovk <liam.vovk@gmail.com>
2017-05-11 19:43:44 -0400

    remove constants folder

--- constants/success.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Liam Vovk on 2017-05-09.
- */
-
-module.exports.USER_CREATED = 'The user was successfully created.'
-module.exports.CHAT_CREATED = 'The chat was successfully created.'
-module.exports.MESSAGE_CREATED = 'The message was successfully created.'
