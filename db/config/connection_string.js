4b5c5b62e846eb4fd930d43413f98784cf89e909
Liam Vovk <liam.vovk@gmail.com>
2017-05-11 19:42:55 -0400

    Move finding connection string based on environment in db config file

---
+++ db/config/connection_string.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Liam Vovk on 2017-05-11.
+ */
+process.env.NODE_ENV = process.env.NODE_ENV.trim()
+let environment = process.env.MONGODB_CONNECTION_PROD
+if (process.env.NODE_ENV === 'test') {
+  environment = process.env.MONGODB_CONNECTION_TEST
+} else if (process.env.NODE_ENV === 'dev') {
+  environment = process.env.MONGODB_CONNECTION_DEV
+}
+
+module.exports = environment
--- db/connect/connect.js
+++ db/connect/connect.js
@@ -4,14 +4,7 @@
 
 require('dotenv').config()
 let mongoose = require('mongoose')
+let connectionString = require('../config/connection_string')
 mongoose.Promise = require('bluebird')
-process.env.NODE_ENV = process.env.NODE_ENV.trim()
-let dbConnectionString = process.env.MONGODB_CONNECTION_PROD
-if (process.env.NODE_ENV === 'test') {
-  dbConnectionString = process.env.MONGODB_CONNECTION_TEST
-} else if (process.env.NODE_ENV === 'dev') {
-  dbConnectionString = process.env.MONGODB_CONNECTION_DEV
-}
-console.log(process.env.NODE_ENV)
 
-module.exports = mongoose.connect(dbConnectionString)
+module.exports = mongoose.connect(connectionString)
