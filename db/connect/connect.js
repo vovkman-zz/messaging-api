018b2cf5b4bf61d6b32b6568bdc09fd714edbce3
Liam Vovk <liam.vovk@gmail.com>
2017-06-08 22:30:15 -0400

    Remove un necessary .env load

--- db/connect/connect.js
+++ db/connect/connect.js
@@ -2,7 +2,6 @@
  * Created by Liam Vovk on 2017-05-04.
  */
 
-require('dotenv').config()
 let mongoose = require('mongoose')
 let connectionString = require('../config/connection_string')
 mongoose.Promise = require('bluebird')
